02723b24629a6a392a6fe1bfe77a64dfc1182f90
Sleep1ngForest <143532490+Sleep1ngForest@users.noreply.github.com>
2024-02-26 00:10:01 +0300

    first commit

--- program.cs
+++ program.cs
@@ -20,3 +20,47 @@ string[] FillArray()
     return workArray;
 }
 
+string PrintArray(string[] workArray)
+{
+    string stringArray = "[";
+    for (int i = 0; i < workArray.Length; i++)
+    {
+        if (i == workArray.Length - 1)
+        {
+            stringArray += $"\"{workArray[i]}\"";
+            break;
+        }
+        stringArray += ($"\"{workArray[i]}\", ");
+    }
+    stringArray += "]";
+    return stringArray;
+}
+
+int CountStringSymbols(string[] workArray)
+{
+    int counter = 0;
+    foreach (string item in workArray)
+    {
+        if (item.Length <= 3)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+string[] GenerateNewArray(string[] workArray)
+{
+    int resultArrayLength = CountStringSymbols(workArray);
+    string[] resultArray = new string[resultArrayLength];
+    int i = 0;
+    foreach (string item in workArray)
+    {
+        if (item.Length <= 3)
+        {
+            resultArray[i] = item;
+            i++;
+        }
+    }
+    return resultArray;
+}
